e5930e1436d21f96ac05d6c143b6b9257de3c11d
Ivan Chukhalo <ivanchukhalo@gmail.com>
2025-08-31 13:08:46 +0300

    feat: add draft for issued loans and payments  routes

---
+++ src/routes/issuedLoansRouters.js
@@ -0,0 +1,9 @@
+import Router from 'express';
+
+const issuedLoansRouter = Router();
+
+issuedLoansRouter.get('/issuedLoans', (req, res) => res.send('All issued loans'));
+issuedLoansRouter.get('/issuedLoans/:id', (req, res) => res.send(`Return issued loan by their ID`));
+issuedLoansRouter.post('/issuedLoans', (req, res) => res.send('Create loan'));
+issuedLoansRouter.put('/issuedLoans/:id', (req, res) => res.send('Update info about an issued loan'));
+issuedLoansRouter.delete('/issuedLoans/:id', (req, res) => res.send('Delete a loan'));
---
+++ src/routes/paymentsRouters.js
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+
+const paymentsRouter = Router();
+
+paymentsRouter.get('/payments', (req, res) => res.send('All payments'));
+paymentsRouter.get('/payments/:id', (req, res) => res.send(`Return the payment by its ID`));
+paymentsRouter.post('/payments', (req, res) => res.send('Add new payment to the records'));
+paymentsRouter.put('/payments/:id', (req, res) => res.send('Update info about the payment'));
+paymentsRouter.delete('/payments/:id', (req, res) => res.send('Delete payment record'));
