6a7d89becf07d047a8ac1df5a2685a157eac046c
Ivan Chukhalo <ivanchukhalo@gmail.com>
2025-09-02 23:20:21 +0300

    feat: add PUT controller to edit client's info

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -24,16 +24,41 @@ export const getClientByID = async (req, res) => {
 // POST: add new client to database
 export const addClient = async (req, res) => {
   try {
-    const { name, phone, contactPerson, } = req.body;
-    const newClient = new BankClient({ name, phone, contactPerson, });
+    const { name, phone, contactPerson } = req.body;
+    const newClient = new BankClient({ name, phone, contactPerson });
     await newClient.save();
-    res
-      .status(201)
-      .json({
-        message: `Client ${name} is succesfully created`,
-        client: newClient,
-      });
+    res.status(201).json({
+      message: `Client ${name} is succesfully created`,
+      client: newClient,
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+};
+
+// PUT: update client data
+export const editClient = async (req, res) => {
+  try {
+    const clientID = req.params.id;
+    const newData = req.body;
+
+    const updatedClient = await BankClient.findByIdAndUpdate(
+      clientID,
+      newData,
+      {
+        new: true,
+        upsert: false,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedClient) {
+      // mongo returns Null if can't find the client by ID
+      res.status(404).json({ message: `Can't find the client` });
+    }
+
+    res.json({ message: `Client ${updatedClient.name} updated successfully` });
+  } catch (err) {
+    res.status(500).json({ error: "Could not edit the client" });
   }
 };
--- src/routes/clientsRouter.js
+++ src/routes/clientsRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getAllClients, getClientByID, addClient } from "../controllers/clientController.js";
+import { getAllClients, getClientByID, addClient, editClient } from "../controllers/clientController.js";
 
 const clientsRouter = Router();
 
 clientsRouter.get('/', getAllClients);
 clientsRouter.get('/:id', getClientByID);
 clientsRouter.post('/', addClient);
-clientsRouter.put('/:id', (req, res) => res.send('Update info about a client recived ID'));
+clientsRouter.put('/:id', editClient);
 clientsRouter.delete('/:id', (req, res) => res.send('Delete the client with ID'));
 
 export default clientsRouter;
