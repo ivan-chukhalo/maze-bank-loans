198b0a4ca4bbbb635cafe658d3c109f7db3a62a9
Ivan Chukhalo <ivanchukhalo@gmail.com>
2025-09-02 22:31:06 +0300

    feat: add GET controller to return a client by ID

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -2,10 +2,22 @@ import BankClient from "../models/BankClient.js";
 
 // GET: return all clients
 export const getAllClients = async (req, res) => {
-    try {
-        const clients = await BankClient.find(); 
-        res.json(clients);
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
+  try {
+    const clients = await BankClient.find();
+    res.json(clients);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
+
+// GET: return client by their ID
+export const getClientByID = async (req, res) => {
+  try {
+    const clientID = req.params.id;
+    const theClient = await BankClient.findById(clientID);
+    res.json(theClient);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
--- src/routes/clientsRouter.js
+++ src/routes/clientsRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getAllClients } from "../controllers/clientController.js";
+import { getAllClients, getClientByID } from "../controllers/clientController.js";
 
 const clientsRouter = Router();
 
 clientsRouter.get('/', getAllClients);
-clientsRouter.get('/:id', (req, res) => res.send(`Return client by their ID`));
+clientsRouter.get('/:id', getClientByID);
 clientsRouter.post('/', (req, res) => res.send('Create new client'));
 clientsRouter.put('/:id', (req, res) => res.send('Update info about a client recived ID'));
 clientsRouter.delete('/:id', (req, res) => res.send('Delete the client with ID'));
